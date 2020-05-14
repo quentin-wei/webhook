12fc97934c1e65d4868d7697310ec738d7b2b4ba
weiguitie@163.com <weiguitie@163.com>
2020-05-14 15:34:23 +0800

    change code

--- font.sh
+++ font.sh
@@ -6,8 +6,10 @@ git reset --hard origin/master
 git clean -f
 echo "拉取新代码"
 git pull origin master
+echo "更新项目依赖"
+yarn install
 echo "编译build"
-npm run build
+yarn build
 echo "开始执行构建后端项目:back为docker镜像名称 1.0为版本号"
 docker build -t font:1.0 .
 echo "停止旧容器 并删除旧容器"
